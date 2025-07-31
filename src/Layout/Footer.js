b9d1327e93492b3ac3bc96d6dc8c0c9c777a68f2
unknown <qkdrk7777775@gmail.com>
2025-07-31 12:49:45 +0900

    footer 수정

--- src/Layout/Footer.js
+++ src/Layout/Footer.js
@@ -2,8 +2,35 @@ function Footer() {
   return (
     <>
       <footer>
-        <div></div>
-        <div></div>
+        <div>
+          <ul>
+            <li className="footer-li-title">
+              <a href="/">company</a>
+            </li>
+            <li>
+              <a href="/">한눈에 보기</a>
+            </li>
+            <li>
+              <a href="/">스타벅스 사명</a>
+            </li>
+            <li>
+              <a href="/">스타벅스 소개</a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <li className="footer-li-title">
+              <a href="/">corporate sales</a>
+            </li>
+            <li>
+              <a href="/">단체 및 기업 구매 안내</a>
+            </li>
+            <li>
+              <a href="/">단체 주문 배달 안내</a>
+            </li>
+          </ul>
+        </div>
       </footer>
     </>
   );
